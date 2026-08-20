b4a750c12b9bae10f8559cc31efb39605654188c
tcr980328 <tcr980328@gmail.com>
2026-08-20 23:11:21 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-
   basePath: isGitHubPages ? "/mole-rush" : "",
   assetPrefix: isGitHubPages ? "/mole-rush" : "",
-};
+} as const;
 
 export default nextConfig;
