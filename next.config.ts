333e0122453bafff4e7810271e2242f1d7d7ff7e
tcr980328 <tcr980328@gmail.com>
2026-08-20 23:33:38 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-
-  // GitHub Pages 的 JS / CSS / _next 資源需要此前綴
   assetPrefix: isGitHubPages ? "/mole-rush" : "",
 } as const;
 
